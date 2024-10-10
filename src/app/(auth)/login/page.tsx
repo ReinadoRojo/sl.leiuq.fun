bd2017bea4b61706241a313d399efa9c9df2f413
Ezequiel <e.cf@leiuq.fun>
2024-10-10 12:42:25 +0200

    auth page

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from "./_components/sign-in";
+
+export default function Page() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <LoginForm />
+    </main>
+  );
+}
