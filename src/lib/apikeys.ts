dabe2df052bcb9a1504628bb35f1833353969e9d
Ezequiel <e.cf@leiuq.fun>
2024-10-11 10:13:48 +0200

    just formatting

--- src/lib/apikeys.ts
+++ src/lib/apikeys.ts
@@ -1,17 +1,22 @@
-import { apiTokens, db } from "@/db/schema"
-import { eq } from "drizzle-orm"
+import { apiTokens, db } from "@/db/schema";
+import { eq } from "drizzle-orm";
 
 export const getApiKeysByOwnerId = async (ownerId: string) => {
-    const dbResult = await db.select({
-        apiToken: apiTokens.apiToken
-    }).from(apiTokens).where(eq(apiTokens.ownerId, ownerId)).execute()
-    if(dbResult.length === 0) return []
-
-    let censoredTokens: Array<any> = [];
-    dbResult.forEach(tkn => {
-        const censoredToken = tkn.apiToken.substring(0, 5) + '*'.repeat(tkn.apiToken.length - 5);
-        censoredTokens.push({ apiToken: censoredToken });
+  const dbResult = await db
+    .select({
+      apiToken: apiTokens.apiToken,
     })
+    .from(apiTokens)
+    .where(eq(apiTokens.ownerId, ownerId))
+    .execute();
+  if (dbResult.length === 0) return [];
 
-    return censoredTokens;
-}
+  let censoredTokens: Array<any> = [];
+  dbResult.forEach((tkn) => {
+    const censoredToken =
+      tkn.apiToken.substring(0, 5) + "*".repeat(tkn.apiToken.length - 5);
+    censoredTokens.push({ apiToken: censoredToken });
+  });
+
+  return censoredTokens;
+};
