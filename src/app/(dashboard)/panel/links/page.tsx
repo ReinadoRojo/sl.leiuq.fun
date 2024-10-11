65d3fcb9b5bc1adcb1feabc49e16131a90583383
Ezequiel <e.cf@leiuq.fun>
2024-10-11 10:13:22 +0200

    refactor links page

--- src/app/(dashboard)/panel/links/page.tsx
+++ src/app/(dashboard)/panel/links/page.tsx
@@ -1,23 +1,13 @@
 import { auth } from "@/auth";
 import { notFound } from "next/navigation";
-import { SelectApiKey } from "./_components/select-key";
-import { getUserByEmail } from "@/lib/users";
-import { getApiKeysByOwnerId } from "@/lib/apikeys";
 import { TableLinks } from "./_components/links-table";
 
 const LinksPage = async () => {
   const session = await auth();
   if (!session?.user) return notFound();
 
-  const user = await getUserByEmail(session.user?.email as string);
-  if (!user) return notFound();
-
-  const userId = user.id;
-  const apiKeyList = await getApiKeysByOwnerId(userId);
-
   return (
     <main className="h-screen pt-28 px-8">
-      <SelectApiKey apiKeyList={apiKeyList} />
       <TableLinks session={session} />
     </main>
   );
