ab3216a8b38d34839c7bb2fee6248a512a611729
Ezequiel C. <62150172+ReinadoRojo@users.noreply.github.com>
2024-12-10 15:21:36 +0100

    added PRIVATE_ROUTES for routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,3 +1,4 @@
 export const ROOT = "/";
 export const PUBLIC_ROUTES = ["/", "/login"];
 export const DEFAULT_REDIRECT = "/panel";
+export const PRIVATE_ROUTES = ["/panel/links", "/panel", "/panel/tokens", "/panel/settings"];
