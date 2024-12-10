50fe5ff3e23f499df9eb39a5fa8769ea64cc0c7c
Ezequiel C. <62150172+ReinadoRojo@users.noreply.github.com>
2024-12-10 15:25:37 +0100

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
 import NextAuth from "next-auth";
 import { authConfig } from "@/auth.config";
-import { DEFAULT_REDIRECT, PUBLIC_ROUTES, ROOT } from "@/routes";
+import { DEFAULT_REDIRECT, PUBLIC_ROUTES, ROOT, PRIVATE_ROUTES } from "@/routes";
 
 const { auth } = NextAuth(authConfig);
 
@@ -9,11 +9,12 @@ export default auth((req) => {
 
   const isAuthenticated = !!req.auth;
   const isPublicRoute = PUBLIC_ROUTES.includes(nextUrl.pathname);
+  const isPrivateRoute = PRIVATE_ROUTES.includes(nextUrl.pathname);
 
   if (isPublicRoute && isAuthenticated)
     return Response.redirect(new URL(DEFAULT_REDIRECT, nextUrl));
 
-  if (!isAuthenticated && !isPublicRoute)
+  if (isPrivate && !isAuthenticated)
     return Response.redirect(new URL(ROOT, nextUrl));
 });
 
